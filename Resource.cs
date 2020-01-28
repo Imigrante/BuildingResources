bc22fb55604820bc59966438d10f709058a4ba90
Leonardo Sugahara <53226260+Imigrante@users.noreply.github.com>
2020-01-28 17:19:24 -0300

    Add files via upload

---
+++ Resource.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine; // Import the UnityEngine's library
+
+public class Resource : MonoBehaviour {
+
+    public string resourceName; // Resource's name
+    public int life; // Resource's Life
+    public int quantityValue; // Value of resources that will be added to inventory
+    public ResourcesManager resourcesManager; // ResourcesManager's script
+    public CharactersManager charactersManager; // CharacterManager's script (or the player's script, if there is only on character controlled by the player)
+
+    void Start() { 
+
+        resourcesManager = GameObject.Find("BuildingSystem").GetComponent<ResourcesManager>(); // Find the ResourcesManager's script in a BuildingSystem's GameObject and assign it in the resourcesManager
+        charactersManager = GameObject.Find("Manager").GetComponent<CharactersManager>(); // Find the CharactersManager's script in a Manager's GameObject and assign it in the charactersManager
+    }
+
+    void FixedUpdate() {
+
+        VerifyLife(); // Call the VerifyLife's function
+    }
+
+    void AddResourceToManager(string name, int value) { // Add a resource to the player's inventory
+
+        resourcesManager.AddResourceToGroup(name, value); // Call the "AddResourceToGroup" function in the ResourcesManager's script to add the resource to the player's inventory
+
+        charactersManager.SetUnitCuttingBool(false); // Call the "SetUnitCuttingBool" function in the CharactersManager's script to cancel the player's interaction animation with the resource
+        charactersManager.RemoveActualResource(); // Call the "RemoveActualResource" function in the CharactersManager's script to remove the target on this resource
+
+        Destroy(this.gameObject); // Destroy this GameObject (the resource)
+    }
+
+    void VerifyLife() { // Resource life's condition
+
+        if (life <= 0) {
+
+            AddResourceToManager(resourceName, quantityValue); // Call the "AddResourceToManager" function to add that resource to the player's inventory
+        }
+    }
+
+    public void TakeDamage(int damage) { // Decreases that resource's life
+
+        life -= damage;
+
+        print(life);
+    }
+}
---
+++ ResourcesManager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine; // Import the UnityEngine's library
+using TMPro; // Import the TMPro's library
+
+public class ResourcesManager : MonoBehaviour {
+
+    [Header("Resources Quantities")] //Shows the name of that group in the Inspector
+    public int woodQuantity; // A int variable that contains the quantity of wood
+    public int stoneQuantity; // A int variable that contains the quantity of stone
+    //public int ironQuantity; // A int variable that contains the quantity of iron
+
+    [Header("Text GameObjects")] //Shows the name of that group in the Inspector
+    public TextMeshProUGUI woodTxt; // A text object that show the quantity of wood
+    /*public TextMeshProUGUI stoneTxt; // A text object that show the quantity of stone
+    public TextMeshProUGUI ironTxt;*/ // A text object that show the quantity of iron
+
+    void FixedUpdate() {
+
+        woodTxt.text = "Wood: " + woodQuantity; //Shows the quantity of wood in the UI
+        /*stoneTxt.text = "Stone: " + stoneQuantity; //Shows the quantity of stone in the UI
+        ironTxt.text = "Stone: " + ironQuantity; */ //Shows the quantity of iron in the UI
+    }
+
+    public void AddResourceToGroup(string name, int quantity) { // This function adds an quantity of a given resource to the player's inventory
+
+        if (name == "Wood") {
+
+            woodQuantity += quantity;
+
+        } else if (name == "Stone") {
+
+            stoneQuantity += quantity;
+
+        } /*else if (name == "Iron") {
+
+            ironQuantity += quantity;
+        }*/
+    }
+
+    public void SubstractResources(string ResourceName, int resourcesQuantity) { // This function removes an quantity of a given resource from the player's inventory
+
+        if (ResourceName == "Wood") {
+
+            woodQuantity -= resourcesQuantity;
+
+        } else if (ResourceName == "Stone") {
+
+            stoneQuantity -= resourcesQuantity;
+
+        } /*else if (ResourceName == "Iron") {
+
+            ironQuantity -= resourcesQuantity;
+        }*/
+    }
+}
